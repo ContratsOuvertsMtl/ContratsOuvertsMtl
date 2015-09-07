c131761047a691993cf7ca63fc1208b22274810b
ContratsOuvertsMtl <ContratsOuvertsMtl@gmail.com>
2015-09-07 13:42:36 -0400

    Update envoyer_twit.py

--- envoyer_twit.py
+++ envoyer_twit.py
@@ -2,9 +2,7 @@
 # coding: utf8
 
 """
-Télécharger les nouveaux fichiers PDF de l'ordre du jour sur le site de la Ville de Montréal
-
-Version 1.1, 2015-08-22
+Version 3.0, 2015-09-07
 Développé en Python 3.4
 Licence CC-BY-NC 4.0 Pascal Robichaud, pascal.robichaud.do101@gmail.com
 """
@@ -12,16 +10,21 @@ Licence CC-BY-NC 4.0 Pascal Robichaud, pascal.robichaud.do101@gmail.com
 
 from twitter import *
 import datetime
-
+from left import *
+        
+        
 def envoyer_twit(message):
 
-    message = message.strip() + str(datetime.datetime.now())
-
-    t = Twitter(auth=OAuth(
-            consumer_key='',
-            consumer_secret='',
-            token='',
-            token_secret=''     
-            ))
+    if message:
     
-    t.statuses.update(status = message)
+        message = message.strip()
+        message = left(message, 140)
+        
+        t = Twitter(auth=OAuth(
+                consumer_key='hu9JvN5KILdb5KTnQhmrvn2sY',
+                consumer_secret='7SsnNhnTF7u0sWlGkdYFf15h22h7VCViTq38JjfqVdIom2OiFE',
+                token='3431920684-Y2yDx74o5XG0GunDLLVyV47W01HHDK6KLZtST0j',
+                token_secret='eudWwmgWcpp6GO5P9sNHAO0SkauK3Dxi35dJWKE16VMRb'     
+                ))
+ 
+        t.statuses.update(status = message)
