50949e843b8613b118c9354a64093187367b9a61
ContratsOuvertsMtl <ContratsOuvertsMtl@gmail.com>
2015-10-02 20:37:18 -0400

    Create mid.py

---
+++ mid.py
@@ -0,0 +1,18 @@
+#!c:\python34\python.exe
+# coding: utf8
+
+
+"""
+Version 4.0, 2015-10-02
+Développé en Python 3.4
+Licence CC-BY-NC 4.0 Pascal Robichaud, pascal.robichaud.do101@gmail.com
+"""
+
+#Fonction mid
+def mid(s, offset, amount):
+
+    return s[offset:offset+amount]
+
+        
+if __name__ == '__main__':
+    mid()
